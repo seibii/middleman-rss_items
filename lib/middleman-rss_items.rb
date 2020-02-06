f614c83442d1c390536e20a3c6c993b5ba5b9493
Yuji Ueki <unhappychoice@gmail.com>
2020-02-07 01:31:01 +0900

    :+1: Implement gem

--- lib/middleman-rss_items.rb
+++ lib/middleman-rss_items.rb
@@ -1,6 +1,24 @@
-require "middleman-core"
+require 'feedjira'
+require 'middleman-core'
+require 'net/http'
 
-Middleman::Extensions.register :middleman-rss_items do
-  require "my-extension/extension"
-  MyExtension
+class MiddlemanRssItems < ::Middleman::Extension
+  option :rss_resources, false, 'RSS resources to fetch'
+  attr_reader :rss_items
+  expose_to_template :rss_items
+
+  def initialize(app, options_hash={}, &block)
+    super
+
+    @rss_items = {}
+  end
+
+  def after_configuration
+    options.rss_resources.each do |resource|
+      rss = Net::HTTP.get(URI.parse(resource[:url]))
+      @rss_items[resource[:name] || resource[:url]] ||= Feedjira.parse(rss).entries
+    end
+  end
 end
+
+Middleman::Extensions.register :rss_items, MiddlemanRssItems
